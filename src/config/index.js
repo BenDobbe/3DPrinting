a33e1e252c6fa21445d049c3ebccc9a7796fac8a
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-03-29 01:49:38 +0100

    new wordpress

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
     /* Replace with online URL */
-    BASE_URL: 'http://ahs-prod-web-3dgdm.azurewebsites.net/wordpress/wp-json/wp/v2',
+    BASE_URL: 'https://ahs-prod-web-3dgdm.azurewebsites.net/wordpress/wp-json/wp/v2',
     MAP_ACCESS_TOKEN: 'pk.eyJ1IjoiY2VkcnZhbmgiLCJhIjoiY2p0ZDJhdWs3MTA4aDQzcDh4Y3YxMW5jMCJ9.Tl2PgWAaWhFTO-Ng2ZUsGw',
     INSTAGRAM_TOKEN: '11172890258.bb87f28.120f385cf7d241598ddda7fc4770bef3'
 };
--- wordpress/wp-content/plugins/photography-portfolio/public/templates/single/protected-portfolio-entry.php
+++
@@ -1,6 +0,0 @@
-<div <?php phort_class( 'PP_Protected' ) ?>>
-
-	<h1 class="PP_Protected__title"><?php esc_html_e( 'Protected Gallery', 'photography-portfolio' ); ?></h1>
-	<?php echo get_the_password_form(); ?>
-
-</div>
